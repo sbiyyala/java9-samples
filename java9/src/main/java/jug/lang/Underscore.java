df6c2054edb474b566fd7b8b85da6428c78df0d3
Shishir Biyyala <Shishir.Biyyala@lab49.com>
2017-10-29 12:07:10 -0400

    [lang]: underscore

---
+++ java9/src/main/java/jug/lang/Underscore.java
@@ -0,0 +1,13 @@
+package jug.lang;
+
+public class Underscore {
+
+    public static void main(String[] args) {
+        // underscore will possibly be used for function parameters you don't care about
+        // [throwaway param, like in javascript]
+
+        // int _ = 10;
+        int ten = 10;
+        System.out.println(ten);
+    }
+}
---
+++ prej9/src/main/java/jug/pre/lang/Underscore.java
@@ -0,0 +1,9 @@
+package jug.pre.lang;
+
+public class Underscore {
+
+    public static void main(String[] args) {
+        int _ = 10;
+        System.out.println(_*10);
+    }
+}
