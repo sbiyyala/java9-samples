d92762c707a752364519608ba3b0540e7d87c33f
megbeckim <timothy@Timothys-Air.fios-router.home>
2017-11-03 08:24:09 -0400

    provided another example of takeWhile

--- java9/src/main/java/net/nycjava/java9/api/stream/J9TakeWhileDemo.java
+++ java9/src/main/java/net/nycjava/java9/api/stream/J9TakeWhileDemo.java
@@ -14,5 +14,36 @@ public class J9TakeWhileDemo {
                         .takeWhile(x -> x < 5)
                         .collect(toList())
         );
+
+
+        // SB: in your example the values are moving in a single direction (ascending), which means that filter()
+        // works just as well as a takeWhile()
+        // instead, consider a data set that has no single direction, e.g., daily temperatures
+
+        List<Integer> maxDailyTempJan2016 = List.of(42, 40, 45, 36, 29, 41, 46, 46, 47, 59, 40, 44, 30, 38, 51,
+                52, 42, 31, 28, 37, 36, 30, 27, 35, 39, 48, 47, 42, 41, 39, 56);
+
+        // java 8
+
+        {
+            int numberOfDaysBeforeTheTemperatureReached50 = 0;
+            while (maxDailyTempJan2016.get(numberOfDaysBeforeTheTemperatureReached50) < 50) {
+                numberOfDaysBeforeTheTemperatureReached50++;
+            }
+
+            System.out.println(numberOfDaysBeforeTheTemperatureReached50);
+        }
+
+        // java 9
+
+        {
+            long numberOfDaysBeforeTheTemperatureReached50 = maxDailyTempJan2016.stream()
+                    .takeWhile(temp -> temp < 50)
+                    .count();
+
+            System.out.println(numberOfDaysBeforeTheTemperatureReached50);
+        }
+
+        // feel free to use this example, or make up another
     }
 }
