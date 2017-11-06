b4cce1b6a3854c9e42972404cc585e1fd67a6833
tfagan <timothy.fagan@lab49.com>
2017-11-06 12:44:35 -0500

    startPipeline

--- java9/src/main/java/net/nycjava/java9/api/J9Process.java
+++ java9/src/main/java/net/nycjava/java9/api/J9Process.java
@@ -4,26 +4,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.Duration;
+import java.util.List;
 
 import static java.lang.String.format;
+import static java.util.Arrays.asList;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public class J9Process {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         // Print all java 9 files. find ~/ to see timeout in play
-        Process process = new ProcessBuilder("find", ".", "-name", "*.java")
-                .start();
+        List<Process> processes = ProcessBuilder.startPipeline(
+                asList(
+                        new ProcessBuilder("find", "."),
+                        new ProcessBuilder("grep", "-E", ".*\\.java")));
 
-        System.out.println(format("PID: %s", process.pid()));
-        process.onExit()
+        Process finalProcess = processes.get(1);
+        
+        finalProcess.onExit()
                 .orTimeout(100, MILLISECONDS)
                 .thenAccept(p -> new BufferedReader(new InputStreamReader(p.getInputStream()))
                         .lines()
                         .filter(l -> l.startsWith("./java9"))
                         .forEach(System.out::println));
-
-        process.waitFor(); // wait till process has terminated
 
         System.out.println(format("CPU: %d ms",
                 ProcessHandle.current().info()
