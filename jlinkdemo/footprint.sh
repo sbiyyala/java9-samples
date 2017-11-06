fe7b2319a5929f7d03158783ccbb660306af5dda
Shishir Biyyala <Shishir.Biyyala@lab49.com>
2017-11-05 22:14:42 -0500

    JRE not JDK size

--- jlinkdemo/footprint.sh
+++ jlinkdemo/footprint.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 java8JreSize=`du -sh $JAVA_8_HOME/jre/`
 jlinkSize=`du -sh jlinkdemo`
-echo "Java8 JDK size:  $java8JreSize"
+echo "Java8 JRE size:  $java8JreSize"
 echo "jlinkdemo size:  $jlinkSize"
 
