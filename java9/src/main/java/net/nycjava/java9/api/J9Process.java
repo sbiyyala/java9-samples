8ee76fe5d45f3f7a568a94687e042144f7d6d461
tfagan <timothy.fagan@lab49.com>
2017-11-06 12:34:04 -0500

    more process fun

--- java9/src/main/java/net/nycjava/java9/api/J9Process.java
+++ java9/src/main/java/net/nycjava/java9/api/J9Process.java
@@ -3,7 +3,9 @@ package net.nycjava.java9.api;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.Duration;
 
+import static java.lang.String.format;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public class J9Process {
@@ -13,8 +15,7 @@ public class J9Process {
         Process process = new ProcessBuilder("find", ".", "-name", "*.java")
                 .start();
 
-        System.out.println(String.format("PID: %s", process.pid()));
-
+        System.out.println(format("PID: %s", process.pid()));
         process.onExit()
                 .orTimeout(100, MILLISECONDS)
                 .thenAccept(p -> new BufferedReader(new InputStreamReader(p.getInputStream()))
@@ -23,5 +24,11 @@ public class J9Process {
                         .forEach(System.out::println));
 
         process.waitFor(); // wait till process has terminated
+
+        System.out.println(format("CPU: %d ms",
+                ProcessHandle.current().info()
+                        .totalCpuDuration()
+                        .map(Duration::toMillis)
+                        .orElse(0L)));
     }
 }
