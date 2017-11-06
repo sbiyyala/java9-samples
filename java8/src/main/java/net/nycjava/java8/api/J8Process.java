8674876a3e6e7129b8a769d7cf92384138f0e62c
Shishir Biyyala <Shishir.Biyyala@lab49.com>
2017-11-06 09:18:34 -0500

    stream over lines

--- java8/src/main/java/net/nycjava/java8/api/J8Process.java
+++ java8/src/main/java/net/nycjava/java8/api/J8Process.java
@@ -6,22 +6,14 @@ import java.io.InputStreamReader;
 
 public class J8Process {
 
-    // Find all java9 files in current dir
+    // Find all java8 files in current dir
     public static void main(String[] args) throws IOException {
         Process process = new ProcessBuilder("find", ".", "-name", "*.java")
                 .start();
 
-        BufferedReader reader =
-                new BufferedReader(new InputStreamReader(process.getInputStream()));
-        StringBuilder builder = new StringBuilder();
-        String line = null;
-        while ((line = reader.readLine()) != null) {
-            if (line.startsWith("./java8")) {
-                builder.append(line);
-                builder.append(System.getProperty("line.separator"));
-            }
-        }
-
-        System.out.println(builder.toString());
+        new BufferedReader(new InputStreamReader(process.getInputStream()))
+                .lines()
+                .filter(l -> l.startsWith("./java9"))
+                .forEach(System.out::println);
     }
 }
